96e7379e1802de85b0daf343f1f2e77563e9013a
Cody Brunner <rockchalkwushock@icloud.com>
2016-08-10 14:42:43 -0500

    V1.6 Quiz App

--- js/app.js
+++ js/app.js
@@ -19,9 +19,12 @@
 /* ---------- Initialization of Global Variables ---------- */
 // ###########################################################
 
-var counter = 0;																																// This will count how many questions have been answered.
-var userGuess = $('input:checked').val(); 																			// Stores user's radio value input.
+//var counter = 0;																																// This will count how many questions have been answered.
+var userGuess = $('input[name=answer]:checked').val(); 													// Variable represents the radio button value from user's selection.
+var userFeedback = $('#userFeedback_container')																	// Variable represents the user feedback from processed selection.
 var myQuestions;																																// New Object created from the prototype of object 'QuizBank'.
+var kansasScore = 0;																														// Placeholder for winning team score.
+var mizzouScore = 0;																														// Placeholder for losing team score.
 
 // ##############################################################
 /* -------------------- Classes & Objects -------------------- */
@@ -35,27 +38,33 @@ var myQuestions;																																// New Object created from the p
 
 $(document).ready(initLoad);
 
-// ####################################################
-/* ----------------- Child Function ---------------- */
-// ####################################################
+// #######################################################
+/* ----------------- initLoad Function ---------------- */
+// #######################################################
 function initLoad()
 {
 	// console.log('page load');
 	welcome();
 };
 
+// ######################################################
+/* ----------------- welcome Function ---------------- */
+// ######################################################
+
 function welcome()
 {
-	$('#welcome').show();							// div containing Welcome Page and pics possibly intro video
+	$('#welcome').show();																													// div containing Welcome Page and pics possibly intro video
 	// console.log('welcome page visible');
-	$('#btn_start').show();							// button for user to leave Welcome Page & Start Quiz
+	$('#btn_start').show();																												// button for user to leave Welcome Page & Start Quiz
 	// console.log('start quiz btn visible');
-	$('#quiz_container').hide();					// contains quiz form
+	$('#quiz_container').hide();																									// contains quiz form
 	// console.log('quiz container hidden');
-	$('#scoreboard_container').hide();				// contains scoreboard that tracks user score during quiz
+	$('#scoreboard_container').hide();																						// contains scoreboard that tracks user score during quiz
 	// console.log('scoreboard container hidden');
-	$('#highscore_container').hide();				// contains high score upon completing quiz
+	$('#highscore_container').hide();																							// contains high score upon completing quiz
 	// console.log('highscore container hidden');
+	$('#userFeedback_container').hide();																					// contains user feedback to answer (pop up fades)
+	// console.log('user feedback container hidden');
 
 
 	$('#btn_start').click(function()
@@ -68,6 +77,10 @@ function welcome()
 	});
 };
 
+// ######################################################
+/* ----------------- runGame Function ---------------- */
+// ######################################################
+
 function runGame()
 {
 	console.log('running game');
@@ -75,37 +88,59 @@ function runGame()
 	checkUsersAnswer();
 }
 
+// ###########################################################
+/* ----------------- populateForm Function ---------------- */
+// ###########################################################
+
 function populateForm()
 {
 	myQuestions = new QuestionList(QuizBank);																			// Creates new object 'myQuestions' from the prototype of object 'QuizBank'.
   myQuestions.fetchCurrentQuestion();																						// Fetches current question in array (object) 'QuizBank'.
 };
 
+// ###############################################################
+/* ----------------- checkUsersAnswer Function ---------------- */
+// ###############################################################
+
 function checkUsersAnswer()
 {
   $('#btn_submit').click(function()
   {
-		userGuess=$('input:checked').val();
-		console.log(userGuess);
-		if (userGuess == undefined)																									// User did not select an option.
-    {
-      // no input from user
-      alert("You must pick an answer!");
-    }
-		else if(userGuess == myQuestions.currentQuestion.correctAnswerIndex)				// User's guess matches correctAnswerIndex.
-    {
-      // correct answer
-      alert("You guessed correctly!");																					// Prompt user with success.
-			nextQuestion();																														// Advance to next question.
-    }
-    else if (userGuess != myQuestions.currentQuestion.correctAnswerIndex)				// User's guess does not match correctAnswerIndex.
-    {
-      // not correct answer
-      alert("You guessed incorrectly!");																				// Prompt user with failure.
-			nextQuestion();																														// Advance to next question.
-    }
+		if (myQuestions.currentQuestion !== undefined)															// This will keep the quiz from continuing to iterate through the array after it's data is all used.
+		{
+			userGuess=$('input:checked').val();
+			console.log(userGuess);
+			if (userGuess == undefined)																								// User did not select an option.
+	    {
+	      // no input from user
+	      alert("You must pick an answer!");
+	    }
+			else if(userGuess == myQuestions.currentQuestion.correctAnswerIndex)			// User's guess matches correctAnswerIndex.
+	    {
+	      // correct answer
+				userFeedback.text("Great Job! That's the correct answer!").fadeIn(1000);// Prompt user with positive feedback by displaying in div #userFeedback
+	      userFeedback.fadeOut(1000);																							// Fade prompt out.
+	      kansasScore++;																													// Iterate Kansas Score by 1.
+	      $('.kansas').text(kansasScore);																					// Print iteration to .kansas in index.html.
+				nextQuestion();																													// Advance to next question.
+	    }
+	    else if (userGuess != myQuestions.currentQuestion.correctAnswerIndex)			// User's guess does not match correctAnswerIndex.
+	    {
+	      // not correct answer
+				userFeedback.text("You must be from Missouri...").fadeIn(1000);					// Prompt user with negative feedback by displaying in div #userFeedback
+				userFeedback.fadeOut(1000);																							// Fade prompt out.
+				mizzouScore++;																													// Iterate Mizzou Score by 1.
+				$('.mizzou').text(mizzouScore);																					// Print iteration to .mizzou in index.html.
+				nextQuestion();																													// Advance to next question.
+	    }
+		}
+
   })
 };
+
+// ###########################################################
+/* ----------------- nextQuestion Function ---------------- */
+// ###########################################################
 
 function nextQuestion()
 {
@@ -118,21 +153,24 @@ function resetForm()
 
 };
 
-function counter()
-{
-	counter++;
-};
+// function counter()
+// {
+// 	counter++;
+// };
 
-function scoreBoard()
-{
+// function scoreBoard()
+// {
+//
+// };
 
-};
+
 
 function highScore()
 {
 	$('#highscore_container').show();
 	$('#quiz_container').hide();
 	$('#welcome').hide();
+	newGame();
 };
 
 function newGame()
